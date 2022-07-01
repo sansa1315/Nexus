98e968a68ea1df8cc4c740e85c246042250db3ea
Barly_Espinal <barlyeduardo.espinal-ovalle@viseo.com>
2022-07-01 08:07:54 -0400

    [ADD] detail view info

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <h1>This is an detail page</h1>
+    <div>
+      <p v-if="$route.params.transaction.concept"><b>Concept</b>:  -- {{$route.params.transaction.concept}}</p>
+      <p v-if="$route.params.transaction.ammount"><b>Ammount</b>: {{$route.params.transaction.ammount}}</p>
+      <p v-if="$route.params.transaction.date"><b>Date</b>: {{$route.params.transaction.date}}</p>
+      <p v-if="$route.params.transaction.description"><b>Description</b>: {{$route.params.transaction.description}} </p>
+    </div>
   </div>
 </template>
+
+<script>
+
+export default {
+  data(){
+    return{
+      transaction : {}
+    }
+  },
+  methods:{
+    
+  },
+ 
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,8 @@
   <div class="container">
     
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-     
-    <div class="onHover" v-for="(transaction,i) in transactions" :key="i" > 
+       
+    <div class="onHover" v-for="(transaction,i) in transactions" :key="i" @click="showDetail(transaction)"> 
       <p class="box mb-1">{{transaction.concept}}</p>
     </div>
   </div>
@@ -20,7 +20,8 @@ export default {
     return{
       transactions:[],
       baseUrl:"http://63.135.170.173:5000/",
-      userId:"799acffc-79be-4e6a-bb72-3865c5c91abe",      
+      userId:"799acffc-79be-4e6a-bb72-3865c5c91abe",
+      
     }
   },
   methods:{
@@ -31,7 +32,15 @@ export default {
         this.transactions = response.data.data        
       }).catch(e => console.log(e))
     },
-        
+    showDetail:function (transaction) {
+      this.$router.push({
+        name:"detail",
+        params:{
+          transaction
+        }
+      })
+    },
+    
   },
   computed:{
      
